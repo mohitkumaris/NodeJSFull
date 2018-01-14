92067e15293b9ec16c262bb027573b1ca381f6aa
mohitkumaris <mohitkumaris@gmail.com>
2018-01-14 20:20:39 +0530

    use loadash

--- notes-node/app.js
+++ notes-node/app.js
@@ -3,15 +3,35 @@
 
 const  fs= require('fs');
 const os=require('os');
+const notes=require('./notes');
+const _=require('lodash');
 
 var user=os.userInfo();
 
+console.log(notes.AddNumbers(7,6));
+
+console.log(_.isString('Yeah'));
+
+var FilteredArray=_.uniq([1,2,1,2,3,4,5,6]);
+
+console.log(FilteredArray);
+
+var users = [
+    { 'user': 'fred',   'age': 48 },
+    { 'user': 'barney', 'age': 36 },
+    { 'user': 'fred',   'age': 40 },
+    { 'user': 'barney', 'age': 34 }
+];
+
+console.log(_.sortBy(users,['user','age']));
+
+
 //console.log(user);
 
-fs.appendFile('greetings.txt',`Hello ${user.username}`,function (err) {
+/*fs.appendFile('greetings.txt',`Hello ${user.username}`,function (err) {
 
     if(err){
         console.log('There is some error');
     }
      console.log('File is created with some entry');
-});
+});*/
---
+++ notes-node/notes.js
@@ -0,0 +1,10 @@
+console.log('Starting notes file');
+
+var objNotes={};
+objNotes.AddNumbers=function (x,y) {
+
+    return x+y;
+}
+
+
+module.exports=objNotes;
