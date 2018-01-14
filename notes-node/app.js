77f28c1b4ff80a09cac79d72281dd3c55f8b078b
mohitkumaris <mohitkumaris@gmail.com>
2018-01-14 18:40:01 +0530

    Package josn files

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,8 +2,13 @@
 'use strict'
 
 const  fs= require('fs');
+const os=require('os');
 
-fs.appendFile('greetings.txt','Hello world',function (err) {
+var user=os.userInfo();
+
+//console.log(user);
+
+fs.appendFile('greetings.txt',`Hello ${user.username}`,function (err) {
 
     if(err){
         console.log('There is some error');
