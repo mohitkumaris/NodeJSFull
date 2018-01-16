e604395af6fe3d9dacc192f3c643c65bde22e11a
mohitkumaris <mohitkumaris@gmail.com>
2018-01-17 01:37:08 +0530

    node js async functionality

--- weather-app/playground/async-basics.js
+++ weather-app/playground/async-basics.js
@@ -1,9 +1,25 @@
-console.log('starting up the app');
+
+    console.log('starting up the app');
+
+
+
 
 setTimeout(()=>{
 
     console.log('this will after 2 seconds asychronously')
 },2000);
+
+// ES 6, block level scope using let.
+for(var i=1;i<=5;i++){
+
+    let x=i;
+    setTimeout(() => {
+        console.log(x);
+    }, 3000)
+
+}
+
+console.log('ending up');
 
 setTimeout(()=>{
 
@@ -11,21 +27,8 @@ setTimeout(()=>{
     },0
 );
 
-function print(x) {
 
-    console.log(x);
-}
-// ES 6 block level scope using let.
-for(var i=1;i<=5;i++){
 
-     let x=i;
-        setTimeout(() => {
-            print(x)
-        }, 3000)
-
-}
-
-console.log('ending up');
 
 /*
 In the example above we have
@@ -34,11 +37,14 @@ node api
 callback queue
 event loop
 
-now the callstack is the 1st thing where code goes as it is stack so
+Now the callstack is the 1st thing where code goes as it is stack so
 last in first out works. So synchronous code works as they should
 but when there is async code it goes to node api and then
 goes to callback queue and wait there until all synchronous code finished up
 or call stack is empty. If its not empty async code stays at
 callback queue , as it goes empty event loop take
 the code to call stack and then the callback code executes.
+
+Node uses libuv.org behind the scenes to manage the asynchronous I/O.
+So even single thread node can run code and event loop at the same time.
  */
