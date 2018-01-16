8384bd83b8869c40561d090d03c4108011e40d2d
mohitkumaris <mohitkumaris@gmail.com>
2018-01-17 01:10:11 +0530

    node js async functionality

--- weather-app/playground/async-basics.js
+++ weather-app/playground/async-basics.js
@@ -2,7 +2,7 @@ console.log('starting up the app');
 
 setTimeout(()=>{
 
-    console.log('this will call asychronously')
+    console.log('this will after 2 seconds asychronously')
 },2000);
 
 setTimeout(()=>{
@@ -15,14 +15,30 @@ function print(x) {
 
     console.log(x);
 }
-
+// ES 6 block level scope using let.
 for(var i=1;i<=5;i++){
 
      let x=i;
         setTimeout(() => {
             print(x)
-        }, 2000)
+        }, 3000)
 
 }
 
 console.log('ending up');
+
+/*
+In the example above we have
+call stack
+node api
+callback queue
+event loop
+
+now the callstack is the 1st thing where code goes as it is stack so
+last in first out works. So synchronous code works as they should
+but when there is async code it goes to node api and then
+goes to callback queue and wait there until all synchronous code finished up
+or call stack is empty. If its not empty async code stays at
+callback queue , as it goes empty event loop take
+the code to call stack and then the callback code executes.
+ */
